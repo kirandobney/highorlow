e58deef3c0f1b7ade595b8ea695668aa38886ffb
kirandobney <135818028+kirandobney@users.noreply.github.com>
2023-06-08 11:03:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int guess = 0;
+            int tries = 5;
+            string input;
+
+            Random random = new Random();
+            number = random.Next(100) + 1;
+
+
+            
+            
+                while (number != guess)
+                {
+
+                    Console.WriteLine("guess a number between 1 and 100");
+                    Console.WriteLine("you have " + tries + " tries left");
+                    input = Console.ReadLine();
+                    guess = Convert.ToInt32(input);
+
+                if (tries != 0)
+                {
+
+                    if (guess > number)
+                    {
+                        Console.WriteLine("lower");
+                        guess = 0;
+                        tries = tries - 1;
+                    }
+
+                    else if (guess < number)
+                    {
+                        Console.WriteLine("higher");
+                        guess = 0;
+                        tries = tries - 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("yes");
+                    }
+
+                }
+                else if (tries == 0)
+                {
+                    Console.WriteLine("out of tries");
+                    break;
+                }
+                }
+            
+            Console.ReadKey();
+        }
+    }
+}
